3eb19f73184152708ecedb4b67be43fd70cd40cf
Alexander Bergasov <a.bergasov@city-mobil.ru>
2020-12-06 16:29:41 +0300

    auth middleware test

---
+++ pkg/repository/rights_test.go
@@ -0,0 +1,5 @@
+package repository
+
+//func TestRightsManager_CheckRight(t *testing.T) {
+//	s := InitRightManager("abc", jwtCookie, 100*time.Millisecond)
+//}
--- pkg/repository/session_test.go
+++ pkg/repository/session_test.go
@@ -1,6 +1,8 @@
 package repository
 
 import (
+	"fmt"
+	"net/http"
 	"net/http/httptest"
 	"testing"
 	"time"
@@ -71,6 +73,59 @@ func TestSessionManager_GetUserAndVersion(t *testing.T) {
 		}
 		if uV != cs.expectedUV {
 			t.Errorf("unexpected userVersion here, want %d, got %d", cs.expectedUV, uV)
+		}
+	}
+
+}
+
+func TestSessionManager_AuthMiddleware(t *testing.T) {
+	s := InitSessionManager("abc", jwtCookie, 222*time.Second)
+	token, err := s.CreateSession(666, 666)
+	if err != nil {
+		t.Error("unexpected error here", err.Error())
+	}
+
+	type tM struct {
+		cookieName   string
+		cookieVal    string
+		expectedCode int
+		expectedUid  bool
+		expectedUV   bool
+	}
+
+	cases := []tM{
+		{"asd", "asds", 401, false, false},
+		{jwtCookie, "asds", 401, false, false},
+		{jwtCookie, token, 200, true, true},
+	}
+
+	for _, cs := range cases {
+		c, _ := gin.CreateTestContext(httptest.NewRecorder())
+		c.Request, _ = http.NewRequest("GET", "/", nil)
+		c.Request.Header.Set("Cookie", fmt.Sprintf("%s=%s", cs.cookieName, cs.cookieVal))
+
+		s.AuthMiddleware(c)
+		if c.Writer.Status() != cs.expectedCode {
+			t.Errorf("expected %d, got %d", cs.expectedCode, c.Writer.Status())
+		}
+		if cs.expectedUV {
+			v, exist := c.Get("user_version")
+			if !exist {
+				t.Error("expected user version here")
+			}
+			if v.(int64) != 666 {
+				t.Errorf("invalid user version, vant %d, got %d", 666, v)
+			}
+		}
+
+		if cs.expectedUid {
+			v, exist := c.Get("user_id")
+			if !exist {
+				t.Error("expected user id here")
+			}
+			if v.(int64) != 666 {
+				t.Errorf("invalid user id, vant %d, got %d", 666, v)
+			}
 		}
 	}
 
