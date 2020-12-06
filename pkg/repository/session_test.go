ba3b39c9afcad0e7099d4cc610996318bf2437ef
Alexander Bergasov <a.bergasov@city-mobil.ru>
2020-12-06 16:07:29 +0300

    user v test

--- pkg/repository/session_test.go
+++ pkg/repository/session_test.go
@@ -1,13 +1,15 @@
 package repository
 
 import (
+	"net/http/httptest"
 	"testing"
 	"time"
+
+	"github.com/gin-gonic/gin"
 )
 
 var (
-	jwtCookie    = "test"
-	jwtCookieExp = 2 * time.Minute
+	jwtCookie = "test"
 )
 
 func TestCreateSession(t *testing.T) {
@@ -31,4 +33,45 @@ func TestCreateSession(t *testing.T) {
 	if userID != 0 || version != 0 {
 		t.Errorf("token expired, but check return userId, user %d, version %d", userID, version)
 	}
+}
+
+func TestSessionManager_GetUserAndVersion(t *testing.T) {
+	s := InitSessionManager("abc", jwtCookie, 100*time.Millisecond)
+
+	type cDT struct {
+		uV           interface{}
+		uID          interface{}
+		expectedBool bool
+		expectedUID  int64
+		expectedUV   int64
+	}
+	cases := []cDT{
+		{"adwdaw", "adwdaw", false, 0, 0},
+		{nil, "adwdaw", false, 0, 0},
+		{"adwdaw", nil, false, 0, 0},
+		{int64(2), int64(1), true, int64(1), int64(2)},
+		{int32(2), int64(1), false, 0, 0},
+		{nil, int64(1), false, 0, 0},
+		{int32(2), int32(1), false, 0, 0},
+	}
+	for _, cs := range cases {
+		c, _ := gin.CreateTestContext(httptest.NewRecorder())
+		if cs.uID != nil {
+			c.Set("user_id", cs.uID)
+		}
+		if cs.uV != nil {
+			c.Set("user_version", cs.uV)
+		}
+		uID, uV, ok := s.GetUserAndVersion(c)
+		if ok != cs.expectedBool {
+			t.Errorf("expected %t, got %t here", cs.expectedBool, ok)
+		}
+		if uID != cs.expectedUID {
+			t.Errorf("unexpected userID here, want %d, got %d", cs.expectedUID, uID)
+		}
+		if uV != cs.expectedUV {
+			t.Errorf("unexpected userVersion here, want %d, got %d", cs.expectedUV, uV)
+		}
+	}
+
 }
